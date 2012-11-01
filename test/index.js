701d8f89d70ca237803c281c107cd0f97c24c4b4
Mathieu Bruyen <code@mais-h.eu>
2012-11-01 14:01:18 +0100

    Make more specific assumptions

--- test/index.js
+++ test/index.js
@@ -39,6 +39,12 @@ describe('Paginate', function () {
           done(err);
         }
         assert.equal(3, req.documents.length);
+        assert.equal('Foo2', req.documents[0]);
+        assert.equal('Foo3', req.documents[1]);
+        assert.equal('Foo4', req.documents[2]);
+        assert.equal(1, req.nextIds.length);
+        assert.equal(5, req.nextIds[0]);
+        assert.equal(0, req.previousIds.length);
         done();
       };
       paginate({
